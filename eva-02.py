1e2f470da1e41de49b3f96720585e22c9f78e818
Frolich <wfr0sts@protonmail.com>
2024-07-26 12:04:41 -0300

    .

--- eva-02.py
+++ eva-02.py
@@ -1,48 +1,11 @@
-import Brute
-import Info
+import Subdomain
+import Information
 import Options
-import Scan
-import Vuls
+import Scanners
 import os
 
 while True:
-    print("""
-     :::::::::: :::     :::     :::                        :::::::   :::::::: 
-     :+:        :+:     :+:   :+: :+:                     :+:   :+: :+:    :+:
-     +:+        +:+     +:+  +:+   +:+                    +:+  :+:+       +:+ 
-     +#++:++#   +#+     +:+ +#++:++#++:     +#++:++#+     +#+ + +:+     +#+   
-     +#+         +#+   +#+  +#+     +#+                   +#+#  +#+   +#+     
-     #+#          #+#+#+#   #+#     #+#                   #+#   #+#  #+#      
-     ##########     ###     ###     ###                    #######  ##########
-     
-+---------------------------------------+-----------------------------------------+
-|                Info                   |                  Scan                   |
-+---------------------------------------+-----------------------------------------+
-| * Domain:                             | * Scan-Others                           |
-|     1 - Whois           2 - Host      |  8 - Burp suite            10 - Wpscan  |
-|               3 - Censys              |  9 - Metasploit            11 - W3af    |
-| * DNS:                                | * Vuls                                  |
-|     4 - Dnsenum         5 - Dnsrecon  |       12 - Jsql       13 - DotDotpwn    |
-| * Net:                                |                                         |
-|     6 - Nmap            7 - Wafw00f   |              --( ~ _ ~ ) --             |
-+---------------------------------------+-----------------------------------------+
-|              Subdomain                |                Enumeration              |
-+---------------------------------------+-----------------------------------------+
-| * Crowlers:                           | * Asn:                                  |
-|   14 - Gau       17 - Reflector       |   * Metabigor                           |
-|   15 - Amass     18 - Wayback         | * Javascript:                           |
-| * Validação:                          |                                         |
-|   19 - Htppx     20- Httprobe         | * Endpoints:                            |
-| * Api:                                |                                         |
-|   * - Api git   * - Git dorker        | * Subdirectories:                       |
-|                                       |    8 - Feroxbuster         9 - Wfuzz    |
-+---------------------------------------+-----------------------------------------+
-|                                    Options                                      |
-+---------------------------------------+-----------------------------------------+
-| 30 - Prepare folder                           31 - Order to tools               |
-| 32 - Main                                     33 - Sites                        |
-| 34 - Dorks                                    35 - Config Setup - tools         |
-+---------------------------------------+-----------------------------------------+\n""")
+    Options.banner_main()
 
     domain = str(input("Domain: "))
     decision = int(input("Tools: "))
@@ -50,29 +13,29 @@ while True:
     os.system("clear")
 
     if decision == 1:
-        Info.whois(domain)
+        Information.whois(domain)
     elif decision == 2:
-        Info.host(domain)
+        Information.censys(domain)
     elif decision == 3:
-        Info.censys(domain)
+        Information.host(domain)
     elif decision == 4:
-        Info.dnsenum(domain)
+        Information.dnsenum(domain)
     elif decision == 5:
-        Info.dnsrecon(domain)
+        Information.dnsrecon(domain)
     elif decision == 6:
-        Info.nmap(domain)
+        Information.nmap(domain)
     elif decision == 7:
-        Info.waf(domain)
+        Information.waf(domain)
     elif decision == 8:
-        Brute.feroxbuster(domain)
+        Subdomain.feroxbuster(domain)
     elif decision == 9:
-        Brute.wfuzz(domain)
+        Subdomain.wfuzz(domain)
     elif decision == 16:
-        Scan.burp_suit()
+        Scanners.burp_suit()
     elif decision == 17:
-        Scan.metasploit()
+        Scanners.metasploit()
     elif decision == 18:
-        Scan.wp_scan(domain)
+        Scanners.wp_scan(domain)
     else:
         pass
 
